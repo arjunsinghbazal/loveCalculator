0c72d09dd64706573f14f1ddd2a3bf7af6edd12d
Arjun Singh <arjunsinghbazal@gmail.com>
2023-03-09 13:06:29 +0530

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function Loveper(){
-    var data = Math.random() * 100;
-    data = Math.floor(data);
-    document.getElementById('Lovevalue').value = data+"%";
+    var name=document.getElementById('name').value;
+    var lname=document.getElementById('lname').value;
+    if(name==""){
+        alert("Please enter your name");
+    }else if(name.length<=2){
+        alert('Min length is 3');
+    }
+    else if(!isNaN(name)){
+        alert('Number is not allowed');
+    }
+    if(lname==""){
+        alert("Please enter your lname");
+    }else if(lname.length<=2){
+        alert('Min length is 3');
+    }
+    else if(!isNaN(lname)){
+        alert('Number is not allowed');
+    }
+    var ldata = Math.random() * 100;
+    ldata = Math.floor(ldata);
+    document.getElementById('love').value = ldata+"%";
     }
